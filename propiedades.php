2d0a8b0553287c3ecc34a8d9944e277011efcc69
Alejandro <alexmillet95@gmail.com>
2019-12-13 19:45:01 +0100

    v1.0

--- propiedades.php
+++ propiedades.php
@@ -6,7 +6,7 @@
 	$GLOBALS["host"]     = "localhost";
 	$GLOBALS["database"] = "cliffhanger_bd";
 	$GLOBALS["user"]     = "ch_admin";
-	$GLOBALS["password"] = "alumno";
+	$GLOBALS["password"] = "ch_password";
 
 	$GLOBALS["rutaLogsAdmin"] = "../logs/";
 	$GLOBALS["rutaLogsPublic"] = "logs/";
